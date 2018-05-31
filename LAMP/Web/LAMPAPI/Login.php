14ac76b92ad18c0e724f1b36f64b5284b336a686
ArisaKitagishi <36506640+ArisaKitagishi@users.noreply.github.com>
2018-05-31 19:57:59 -0400

    login.php - update working but not in ARC

--- LAMP/Web/LAMPAPI/Login.php
+++ LAMP/Web/LAMPAPI/Login.php
@@ -1,39 +1,71 @@
 <?php
+	// Get file contents
+	$inData = json_decode(file_get_contents('php://input'), true);
 
-	$inData = getRequestInfo();
-	
+	// Prepare variables to be filled.
 	$id = 0;
 	$firstName = "";
 	$lastName = "";
+ 
+	// Set up connection
+	$hostname = 'localhost';
+	$databaseName = 'contactManager';
+	$username = 'root';
+	$password = 'contactmanager7';
 
-	$conn = new mysqli("localhost", "leinecke_SaRcc", "Wash9Lives!", "leinecke_COP4331");
-	if ($conn->connect_error) 
+	$conn = new mysqli($hostname, $username, $password, $databaseName);
+
+	// Check for connection error.
+	if ($conn->connect_error)
 	{
-		returnWithError( $conn->connect_error );
-	} 
+		error($conn->connect_error);
+	}
+
+	// Interact with database
 	else
 	{
-		$sql = "SELECT ID,firstName,lastName FROM Users where Login='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+		// Select row from User table associated with the user logging in
+		$sql = "SELECT ID,firstName,lastName FROM Users where Username ='" . $inData["username"] . "' and Password ='" . $inData["password"] . "'";
+
+		// Check if the info has made its way into the database.
 		$result = $conn->query($sql);
+
+		// If we have results
 		if ($result->num_rows > 0)
 		{
+			// Fetch the associated information in the row.
 			$row = $result->fetch_assoc();
+
+			// Fill empty variables
 			$firstName = $row["firstName"];
 			$lastName = $row["lastName"];
 			$id = $row["ID"];
 		}
 		else
 		{
-			returnWithError( "No Records Found" );
+			// Query found nothing.
+			error( "No Records Found" );
 		}
 		$conn->close();
-	}
-	
-	returnWithInfo($firstName, $lastName, $id );
+	 }
 
-	function getRequestInfo()
+	// Send empty error.
+	// error("");
+
+	// Return information to the front end
+	 returnWithInfo($firstName, $lastName, $id);
+
+
+	function error( $err )
 	{
-		return json_decode(file_get_contents('php://input'), true);
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+     $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+     sendResultInfoAsJson( $retValue );
 	}
 
 	function sendResultInfoAsJson( $obj )
@@ -41,17 +73,4 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
 ?>
