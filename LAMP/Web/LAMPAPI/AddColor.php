8776b88bd451f5f6778432976591c56f7254d194
ArisaKitagishi <36506640+ArisaKitagishi@users.noreply.github.com>
2018-06-01 16:33:31 -0400

    update addcolor.php

--- LAMP/Web/LAMPAPI/AddColor.php
+++ LAMP/Web/LAMPAPI/AddColor.php
@@ -1,29 +1,101 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$color = $inData["color"];
-	$userId = $inData["userId"];
+	// Get file contents
+	$inData = json_decode(file_get_contents('php://input'), true);
 
-	$conn = new mysqli("localhost", "leinecke_SaRcc", "Wash9Lives!", "leinecke_COP4331");
-	if ($conn->connect_error) 
+	// Take data from front-end
+	$id = $inData["userID"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$workPhone = $inData["workPhone"];
+	$homePhone = $inData["homePhone"];
+	$cellPhone = $inData["mobilePhone"];
+	$address1 = $inData["address1"];
+	$address2 = $inData["address2"];
+	$zip = $inData["zip"];
+	$email = $inData["email"];
+
+
+	$hostname = 'localhost';
+	$databaseName = 'contactManager';
+	$username = 'root';
+	$password = 'contactmanager7';
+
+	// Set up connection
+	$conn = new mysqli($hostname, $username, $password, $databaseName);
+
+	// Check for connection error
+	if ($conn->connect_error)
 	{
-		returnWithError( $conn->connect_error );
-	} 
+		error($conn->connect_error);
+	}
+	// Interact with database
 	else
 	{
-		$sql = "insert into Colors (UserId,Name) VALUES (" . $userId . ",'" . $color . "')";
-		if( $result = $conn->query($sql) != TRUE )
+ 
+		$check = "SELECT friendID, firstName, lastName, workNumber, homeNumber, mobileNumber, address1, address2, zip, email FROM Contacts";
+
+		// Check if the info has made its way into the database.
+		$result = $conn->query($check);
+    
+		// going through all rows to check if we already have the EXACT same contact
+		if ($result->num_rows > 0)
+   {
+     while($row = $result->fetch_assoc()) 
+     {
+     if(	
+         (empty($firstName) ? (empty($firstName) === empty($row["firstName"])) : ($firstName === $row["firstName"])) &&
+         (empty($lastName) ? (empty($lastName) === empty($row["lastName"])) : ($lastName === $row["lastName"])) &&
+         (empty($workPhone) ? (empty($workPhone) === empty($row["workNumber"])) : ($workPhone === $row["workNumber"])) &&
+         (empty($homePhone) ? (empty($homePhone) === empty($row["homeNumber"])) : ($homePhone === $row["homeNumber"])) &&
+         (empty($cellPhone) ? (empty($cellPhone) === empty($row["mobileNumber"])) : ($cellPhone === $row["mobileNumber"])) &&
+         (empty($address1) ? (empty($address1) === empty($row["address1"])) : ($address1 === $row["address1"])) &&
+         (empty($address2) ? (empty($address2) === empty($row["address2"])) : ($address2 === $row["address2"])) &&
+         (empty($zip) ? (empty($zip) === empty($row["zip"])) : ($zip === $row["zip"])) &&
+         (empty($email) ? (empty($email) === empty($row["email"])) : ($email === $row["email"]))
+        )
+        {  
+           $resultCheck = "That contact already exists.";
+           $conn->close();
+        }
+     }
+   }    
+   
+   
+   
+   
+		// Create row in Contacts table
+		$sql = "INSERT INTO Contacts (friendID, firstName, lastName, workNumber, homeNumber, mobileNumber, address1, address2, zip, email) VALUES
+									   ('$id',
+									    '$firstName',
+										'$lastName',
+										'$workPhone',
+										'$homePhone',
+										'$cellPhone',
+										'$address1',
+										'$address2',
+										'$zip',
+										'$email')";
+    $resultCheck = "Added contact!";
+		// Check if the info has made its way into the database.
+		if($result = $conn->query($sql) != TRUE)
 		{
-			returnWithError( $conn->error );
+    $resultCheck = "Error"; 
+			error("Insert Invalid");
 		}
+
+		// Close the connection
 		$conn->close();
+    
 	}
-	
-	returnWithError("");
-	
-	function getRequestInfo()
+
+	// Send empty error
+	// error("");
+
+
+	function error ($err)
 	{
-		return json_decode(file_get_contents('php://input'), true);
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
 	}
 
 	function sendResultInfoAsJson( $obj )
@@ -31,11 +103,4 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
 ?>
